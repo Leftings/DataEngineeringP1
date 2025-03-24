b849f74e1b4cd7ae92a6c642d30483bb34def7c4
Jarno Vis <84909095+Jarnovis@users.noreply.github.com>
2025-03-24 17:11:10 +0100

    Custom charts

---
+++ Frontend/src/DashboardElements/Cards.jsx
@@ -0,0 +1,7 @@
+export function Card({ children }) {
+    return <div className="bg-white shadow-lg p-4 rounded-lg">{children}</div>;
+}
+
+export function CardContent({ children }) {
+return <div>{children}</div>;
+}
