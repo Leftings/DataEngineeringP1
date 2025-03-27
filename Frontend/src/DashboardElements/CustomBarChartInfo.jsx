05cea2c86ac55ca5b1390e3f377e5585aac17a29
Jarno Vis <84909095+Jarnovis@users.noreply.github.com>
2025-03-27 14:15:28 +0100

    Pie chart + update info chart

--- Frontend/src/DashboardElements/CustomBarChartInfo.jsx
+++ Frontend/src/DashboardElements/CustomBarChartInfo.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid, ResponsiveContainer } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid, ResponsiveContainer, Rectangle } from "recharts";
 
 const CustomTooltip = ({ active, payload, label }) => {
   if (active && payload && payload.length) {
     return (
       <div className="custom-tooltip">
-        <p className="label">{`${label} : ${payload[0].value}`}</p>
+        <p className="label">{`${payload[0].payload.item} : ${payload[0].value}`}</p>
       </div>
     );
   }
@@ -17,13 +16,19 @@ const CustomBarChartInfo = ({ name, width, height, data, xAxis, dataKey }) => {
     <div className="chart-container">
       <h2 className="text-xl font-bold text-center mb-4">{name}</h2>
       <ResponsiveContainer width="100%" height={height}>
-        <BarChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-          <CartesianGrid strokeDasharray="3 3" />
+        <BarChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }} style={{ backgroundColor: 'transparent' }} fill="transparent">
+          <CartesianGrid strokeDasharray="3 3" fill="transparent" stroke="#ddd"/>
           <XAxis dataKey={xAxis} />
           <YAxis />
           <Tooltip content={<CustomTooltip />} />
           <Legend />
-          <Bar dataKey={dataKey} barSize={20} fill="#8884d8" />
+          <Bar 
+            dataKey={dataKey}
+            barSize={20}
+            fill="Cyan"
+            activeBar={<Rectangle fill="White" background={null}/>}
+            background={null}
+          />
         </BarChart>
       </ResponsiveContainer>
     </div>
