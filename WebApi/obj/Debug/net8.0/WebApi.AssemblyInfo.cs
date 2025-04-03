13e80a8d8bda880b6c2f428f4fbda276ec0d6b0a
Leftings <leftingss@gmail.com>
2025-04-03 15:07:58 +0200

    EVerything work but no fact

--- WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+097977634b26770e672d4e2daf38fae5470d5f58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1210b9010072804871f0f69d63711ef197e17043")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
