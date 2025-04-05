7c459c5ca8371c1de50b13b2b4c9491478c474cb
Jarno Vis <84909095+Jarnovis@users.noreply.github.com>
2025-04-05 16:15:21 +0200

    Nieuwe data

---
+++ Frontend/src/Selection/Selection.jsx
@@ -0,0 +1,12 @@
+function Selection()
+{
+    return (
+        <>
+            <a href="/PowerBi">PowerBi Dashboard</a>
+            <br></br>
+            <a href="/Custom">Custom Dashboard</a>
+        </>
+    );
+}
+
+export default Selection;
